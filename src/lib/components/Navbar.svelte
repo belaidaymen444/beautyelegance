d2efef461f6f191560d8b06de070824a82e8d775
The Cool News Guy <joewilliamdd31@gmail.com>
2024-04-05 16:34:37 +0100

    Edit Navbar component

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	const navLinksTexts = [
+		'PROMOTION',
+		'MAQUILLAGE',
+		'CORPS & BAIN',
+		'CHEVEUX',
+		'PARFUMS',
+		'SOINS',
+		'DEO & STICK',
+		'A PROPOSE DE NOUS'
+	];
+</script>
+
+<nav>
+	<ul>
+		{#each navLinksTexts as linkText (linkText)}
+			<li><a href="/">{linkText}</a></li>
+		{/each}
+	</ul>
+</nav>
+
+<style>
+	nav ul {
+		display: flex;
+		justify-content: space-around;
+		padding: 2rem;
+		list-style-type: none;
+	}
+
+	li a {
+		color: #000;
+		text-decoration: none;
+		font-size: 2rem;
+		transition: 0.2s color;
+	}
+
+	li a:hover {
+		color: #d1b2b2;
+	}
+</style>
