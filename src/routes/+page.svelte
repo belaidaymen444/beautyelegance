fe159273b0dd71f42358c5dbbee7267d5b11d891
The Cool News Guy <joewilliamdd31@gmail.com>
2024-04-05 16:36:11 +0100

    Append Navbar to header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import HeaderTopContainer from '$lib/components/HeaderTopContainer.svelte';
+	import Navbar from '$lib/components/Navbar.svelte';
 	import '../app.css';
 </script>
 
 <header>
 	<HeaderTopContainer />
+	<Navbar />
 </header>
