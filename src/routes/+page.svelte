85f4b5fcafed6687a9242b74c23c2e56bb47bdf8
The Cool News Guy <joewilliamdd31@gmail.com>
2024-04-07 10:00:08 +0100

    Add a title and a favicon to the app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
 	import HeaderTopContainer from '$lib/components/HeaderTopContainer.svelte';
 	import Navbar from '$lib/components/Navbar.svelte';
+	import logo from '$lib/assets/logo.png';
 	import '../app.css';
 </script>
+
+<svelte:head>
+	<link rel="shortcut icon" href={logo} type="image/x-icon" />
+	<title>Beaute & Elegance</title>
+</svelte:head>
 
 <header>
 	<HeaderTopContainer />
