4a274af6b2f9910e9e58fe021fa53e0df0dd822f
The Cool News Guy <joewilliamdd31@gmail.com>
2024-04-11 23:47:23 +0100

    Load Abrit Fatface and Andada Pro fonts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,6 +11,14 @@
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" />
 	<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap"
+		rel="stylesheet"
+	/>
+	<link
+		href="https://fonts.googleapis.com/css2?family=Andada+Pro:ital@0;1&display=swap"
+		rel="stylesheet"
+	/>
 	<link rel="shortcut icon" href={logo} type="image/x-icon" />
 	<title>Beaute & Elegance</title>
 </svelte:head>
