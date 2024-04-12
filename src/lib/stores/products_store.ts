ab13266bdddd3aacbbab7acd345619f965098673
The Cool News Guy <joewilliamdd31@gmail.com>
2024-04-12 17:12:40 +0100

    Organize products into categories

--- src/lib/stores/products_store.ts
+++ src/lib/stores/products_store.ts
@@ -6,41 +6,46 @@ import photo10 from '$lib/assets/Sample Imgs/Products Imgs/photo_10.jpg';
 import photo11 from '$lib/assets/Sample Imgs/Products Imgs/photo_11.jpg';
 import photo14 from '$lib/assets/Sample Imgs/Products Imgs/photo_14.png';
 
-export const products = writable<ProductProps[]>([
-	{
-		imgUrl: photo1,
-		name: 'nouba',
-		description: 'Rouge à lèvres mat',
-		price: 2000
-	},
-	{
-		imgUrl: photo6,
-		name: 'bourjois',
-		description: 'Fond de teint 123 30ml',
-		price: 950
-	},
-	{
-		imgUrl: photo14,
-		name: 'maybelline',
-		description: 'Rouge à lèvres mat',
-		price: 1400
-	},
-	{
-		imgUrl: photo9,
-		name: 'foo',
-		description: 'Rouge à lèvres mat',
-		price: 2000
-	},
-	{
-		imgUrl: photo10,
-		name: 'nouba',
-		description: 'Fond de teint 123 30ml',
-		price: 950
-	},
-	{
-		imgUrl: photo11,
-		name: 'bourjois',
-		description: 'Rouge à lèvres mat',
-		price: 1400
-	}
-]);
+export const products = writable<Record<string, ProductProps[]>>({
+	maquillage: [
+		{
+			imgUrl: photo10,
+			name: 'nouba',
+			description: 'Fond de teint 123 30ml',
+			price: 950
+		},
+		{
+			imgUrl: photo11,
+			name: 'bourjois',
+			description: 'Rouge à lèvres mat',
+			price: 1400
+		},
+		{
+			imgUrl: photo14,
+			name: 'maybelline',
+			description: 'Rouge à lèvres mat',
+			price: 1400
+		}
+	],
+	others: [
+		{
+			imgUrl: photo1,
+			name: 'nouba',
+			description: 'Rouge à lèvres mat',
+			price: 2000
+		},
+		{
+			imgUrl: photo6,
+			name: 'bourjois',
+			description: 'Fond de teint 123 30ml',
+			price: 950
+		},
+
+		{
+			imgUrl: photo9,
+			name: 'foo',
+			description: 'Rouge à lèvres mat',
+			price: 2000
+		}
+	]
+});
