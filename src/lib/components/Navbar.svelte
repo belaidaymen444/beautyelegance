7a5cfba49d5a492396b16361f0fb6db6c5395209
The Cool News Guy <joewilliamdd31@gmail.com>
2024-04-13 11:29:13 +0100

    Make navbar responsive

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import MediaQuery from 'svelte-media-queries';
+	import closeIcon from '$lib/assets/icons/close.png';
+
 	const navLinksTexts = [
 		'PROMOTION',
 		'MAQUILLAGE',
@@ -10,13 +13,19 @@
 	];
 </script>
 
-<nav>
-	<ul>
-		{#each navLinksTexts as linkText (linkText)}
-			<li><a href="/">{linkText}</a></li>
-		{/each}
-	</ul>
-</nav>
+<MediaQuery query="(max-width: 950px)" let:matches>
+	<nav>
+		{#if matches}
+			<img src={closeIcon} alt="close" class="close-icon" />
+			<div class="seperator"></div>
+		{/if}
+		<ul>
+			{#each navLinksTexts as linkText (linkText)}
+				<li><a href="/">{linkText}</a></li>
+			{/each}
+		</ul>
+	</nav>
+</MediaQuery>
 
 <style>
 	nav ul {
@@ -35,5 +44,63 @@
 
 	li a:hover {
 		color: #d1b2b2;
+	}
+
+	@media screen and (max-width: 950px) {
+		nav {
+			height: 100vh;
+			width: 100%;
+			position: fixed;
+			top: 0;
+			left: 0;
+			background: #fff;
+			z-index: 999;
+		}
+
+		.close-icon {
+			position: absolute;
+			width: 3rem;
+			right: 2rem;
+			top: 2rem;
+			cursor: pointer;
+		}
+
+		.seperator {
+			width: 100%;
+			height: 0.3rem;
+			position: absolute;
+			top: 6rem;
+			left: 0;
+			background: #000;
+		}
+
+		nav ul {
+			flex-direction: column;
+			justify-content: center;
+			gap: 5rem;
+			min-height: 100%;
+			padding: 0;
+			padding-left: 2rem;
+		}
+
+		li a {
+			font-size: 2.5rem;
+		}
+
+		ul li {
+			padding: 1rem;
+			position: relative;
+		}
+
+		ul li::before {
+			content: '';
+			position: absolute;
+			width: 1%;
+			height: 80%;
+			top: 50%;
+			transform: translateY(-50%);
+			left: -1rem;
+			background: #fa9;
+		}
 	}
 </style>
