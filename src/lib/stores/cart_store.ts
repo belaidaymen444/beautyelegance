b98cf52459a20a4a724a7890747306db0b823e66
The Cool News Guy <joewilliamdd31@gmail.com>
2024-04-17 01:04:33 +0100

    Update cart store

--- src/lib/stores/cart_store.ts
+++ src/lib/stores/cart_store.ts
@@ -1,3 +1,44 @@
-import { writable } from 'svelte/store';
+import { writable, type Writable } from 'svelte/store';
 
-export const cart = writable<(ProductProps & { quantity: number })[]>([]);
+export const cart = createCartStore();
+
+function createCartStore(): CartStore {
+	const { subscribe, update } = writable<CartProductProps[]>([]);
+
+	return {
+		subscribe,
+		add(p) {
+			update((prevCart) => {
+				const productInCart = prevCart.find((productInCart) => productInCart.name === p.name);
+
+				if (productInCart) {
+					productInCart.quantity += 1;
+				} else {
+					prevCart.push({ ...p, quantity: 1 });
+				}
+
+				return prevCart;
+			});
+		},
+		remove(p, allQuantites) {
+			update((prevCart) => {
+				const productInCart = prevCart.find((productInCart) => productInCart.name === p.name);
+
+				if (productInCart) {
+					if (allQuantites) {
+						prevCart.splice(prevCart.indexOf(productInCart), 1);
+					} else {
+						productInCart.quantity -= 1;
+					}
+				}
+
+				return prevCart;
+			});
+		}
+	};
+}
+
+interface CartStore extends Pick<Writable<CartProductProps[]>, 'subscribe'> {
+	add: (p: ProductProps) => void;
+	remove: (p: CartProductProps, allQuantites: boolean) => void;
+}
