f7f900e3f19e2078fd9ad685b95769d53b400c4e
Aymen Bahmed <joewilliamdd31@gmail.com>
2024-05-27 08:03:34 +0100

    Move links in routes/+page's head to routes/+layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,20 @@
+<script lang="ts">
+	import logo from '$lib/assets/logo.png';
+</script>
+
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" />
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap"
+		rel="stylesheet"
+	/>
+	<link
+		href="https://fonts.googleapis.com/css2?family=Andada+Pro:ital@0;1&display=swap"
+		rel="stylesheet"
+	/>
+	<link rel="shortcut icon" href={logo} type="image/x-icon" />
+</svelte:head>
+
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,6 @@
 	import Navbar from '$lib/components/Navbar.svelte';
 	import CompaniesSlider from '$lib/components/CompaniesSlider.svelte';
 	import ProductsGrid from '$lib/components/ProductsGrid.svelte';
-	import logo from '$lib/assets/logo.png';
 	import '../app.css';
 	import FeaturesContainer from '$lib/components/FeaturesContainer.svelte';
 	import InfoContainer from '$lib/components/InfoContainer.svelte';
@@ -14,18 +13,6 @@
 </script>
 
 <svelte:head>
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" />
-	<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet" />
-	<link
-		href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap"
-		rel="stylesheet"
-	/>
-	<link
-		href="https://fonts.googleapis.com/css2?family=Andada+Pro:ital@0;1&display=swap"
-		rel="stylesheet"
-	/>
-	<link rel="shortcut icon" href={logo} type="image/x-icon" />
 	<title>Beaute & Elegance</title>
 </svelte:head>
 
