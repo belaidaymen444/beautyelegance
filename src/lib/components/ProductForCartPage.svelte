b73cab6f8451b22ae4471f26dbb983ddce9d77b9
belaidaymen444 <belaidaymen444@gmail.com>
2024-07-21 14:53:56 +0100

    Moved product table rows from +page.svelte to ProductForCartPage.svelte and added responsive design for mobile devices.

--- src/lib/components/ProductForCartPage.svelte
+++ src/lib/components/ProductForCartPage.svelte
@@ -1,8 +1,91 @@
 <script lang="ts">
     import { cart } from "$lib/stores/cart_store";
+    import deletIcon from '$lib/assets/icons/deletcartproduct.svg';
 
 
 </script>
 
 
 
+
+    
+        <tr class="details">
+            <td
+                ><button class="delet-Button"
+                    ><img src={deletIcon} width="13" alt="deletIcon" /></button
+                ></td
+            >
+            <td><img src="" alt="" width="100" /></td>
+            <td class="descraption">
+                GRACIAS Shampoing sans Sulfate à l’huile de figue de Barbarie230ML
+            </td>
+            <td class="Prix">450.00DZD</td>
+            <td>
+                <div class="Qte-general-input">
+                    <input class="leftbt" type="button" value="+" />
+                    <input class="QTE" type="number" value="1" />
+                    <input class="rightbt" type="button" value="-" />
+                </div>
+            </td>
+            <td class="total-Price-X-QTE">900.00 DZD</td>
+        </tr>
+        <tr class="details">
+            <td
+                ><button class="delet-Button"
+                    ><img src={deletIcon} width="13" alt="deletIcon" /></button
+                ></td
+            >
+            <td><img src="" alt="" width="100" /></td>
+            <td class="descraption">
+                GRACIAS Shampoing sans Sulfate à l’huile de figue de Barbarie230ML
+            </td>
+            <td class="Prix">450.00DZD</td>
+            <td>
+                <div class="Qte-general-input">
+                    <input class="leftbt" type="button" value="+" />
+                    <input class="QTE" type="number" value="1" />
+                    <input class="rightbt" type="button" value="-" />
+                </div>
+            </td>
+            <td class="total-Price-X-QTE">900.00 DZD</td>
+        </tr>
+   
+
+<div class="resposive-side">
+    <table class="table-phone">
+        <tr class="top-part">
+            <td>
+                <div>
+                    <button class="delet-Button"
+                        ><img src={deletIcon} width="13" alt="deletIcon" /></button
+                    >
+                    <h1>GRACIAS Shampoing</h1>
+                </div>
+            </td>
+            <td> <img src="" alt="" width="300" /> </td>
+        </tr>
+        <tr class="for-descraption">
+            <td id="descraption" class="descraption">
+                GRACIAS Shampoing sans Sulfate à l’huile de figue de Barbarie230ML</td
+            >
+        </tr>
+        <tr class="detail-in-phone">
+            <td>PRIX : <span class="Prix">450.00DZD</span></td>
+            <td
+                >QUANTITE : <div class="Qte-general-input">
+                    <input class="leftbt" type="button" value="+" />
+                    <input class="QTE" type="number" value="1" />
+                    <input class="rightbt" type="button" value="-" />
+                </div></td
+            >
+            <td>sous-TOTAL : <span class="total-Price-X-QTE">900.00 DZD</span></td>
+        </tr>
+    </table>
+</div>
+
+
+
+<style>
+
+
+</style>
