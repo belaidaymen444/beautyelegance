9f093a66b4df654b765e48bd9a20c33f2e019cbb
belaidaymen444 <belaidaymen444@gmail.com>
2024-07-24 22:21:47 +0100

    Added InfoContainer and FeaturesContainer components to footer

--- src/routes/panier/+page.svelte
+++ src/routes/panier/+page.svelte
@@ -3,6 +3,8 @@
 	import ProductForCartPage from '$lib/components/CartPage/CartPagePrduct.svelte';
 	import ResposiveSideCart from '$lib/components/CartPage/ResponsiveProductCart.svelte';
 	import ConfirmationSideforCartPage from '$lib/components/CartPage/ConfirmationSideforCartPage.svelte';
+	import InfoContainer from '$lib/components/InfoContainer.svelte';
+	import FeaturesContainer from '$lib/components/FeaturesContainer.svelte';
 	import { cart } from '$lib/stores/cart_store';
 	import { products } from '$lib/stores/products_store';
 	let isrunned = false;
@@ -50,6 +52,14 @@
 		</div>
 	</div>
 </main>
+<footer>
+	<h2 class="footer-title">A propose de nous</h2>
+	<div class="container">
+		<InfoContainer />
+		<FeaturesContainer />
+	</div>
+	<div class="copyright-container">&copy; 2024 Beaute & Elegance. All rights reserved.</div>
+</footer>
 
 <style>
 	nav {
@@ -84,10 +94,7 @@
 		padding: 1.5rem;
 		border-bottom: 2px solid rgba(128, 128, 128, 0.185);
 	}
-	.details input[type='number']::-webkit-inner-spin-button,
-	.details input[type='number']::-webkit-outer-spin-button {
-		-webkit-appearance: none;
-	}
+
 	.product-details {
 		display: flex;
 		align-items: start;
@@ -96,13 +103,7 @@
 		margin-bottom: 1rem;
 		width: 92%;
 	}
-	.details {
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		padding: 1rem;
-		border-bottom: 2px solid rgba(128, 128, 128, 0.185);
-	}
+
 	.main-table {
 		display: flex;
 		flex-direction: column;
@@ -120,35 +121,43 @@
 
 		padding-left: 2rem;
 	}
-	.details td {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		margin-right: 2rem;
-	}
-	.delet-Button {
-		display: flex;
-		justify-content: center;
-		background: none;
-		border: none;
-		outline: none;
-		cursor: pointer;
-	}
-	.descraption {
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: center;
-		color: #333333;
-		font-weight: 600;
-		width: 35%;
-	}
 
-	hr {
-		border: solid 1px rgba(128, 128, 128, 0.185);
-	}
+
+
+
 	.resposive-side {
 		display: none;
+	}
+	footer {
+		margin-top: 2rem;
+	}
+
+	.footer-title {
+		font-family: 'Abril Fatface';
+		font-weight: lighter;
+		text-align: center;
+	}
+	footer .container {
+		display: flex;
+		margin-top: 5rem;
+		align-items: start;
+		justify-content: space-evenly;
+		gap: 4rem;
+		flex-wrap: wrap;
+		padding-inline: 3rem;
+	}
+	.copyright-container {
+		margin-top: 8rem;
+		min-height: 5rem;
+		background: #f0d9d9;
+		display: grid;
+		place-content: center;
+	}
+
+	@media (max-width: 365px) {
+		.copyright-container {
+			font-size: 1.3rem;
+		}
 	}
 
 	@media (min-width: 660px) and (max-width: 1050px) {
@@ -163,17 +172,7 @@
 			display: flex;
 			flex-direction: column;
 		}
-		.confirmation-side {
-			display: flex;
-			flex-direction: column;
-			width: 100%;
-		}
-		.confirmation-side .validation {
-			margin-top: 0rem;
-		}
-		.bottom-Confirmation-side {
-			margin-top: 2rem;
-		}
+
 		.add-many-products {
 			overflow-y: auto;
 		}
@@ -183,9 +182,7 @@
 			margin: 0;
 			width: 100%;
 		}
-		.details {
-			padding: 0;
-		}
+	
 	}
 	@media (max-width: 660px) {
 		.product-details {
@@ -207,66 +204,7 @@
 			text-align: center;
 		}
 
-		.top-part {
-			display: flex;
-			flex-direction: column;
-			font-size: 1.2rem;
-			width: 100%;
-			position: relative;
-		}
-		.top-part div {
-			width: 100%;
-			display: flex;
-			align-items: center;
-			text-align: center;
-			justify-content: center;
-		}
-		.top-part div button {
-			position: absolute;
-			left: 5%;
-		}
-		.top-part div h1 {
-			font-size: 2.2rem;
-		}
-		.for-descraption {
-			display: flex;
-			justify-content: center;
-			text-align: center;
-			align-items: center;
-			width: 100%;
-		}
-		#descraption {
-			width: 100%;
-			padding: 2rem;
-			border-bottom: dashed 2px rgba(128, 128, 128, 0.185);
-		}
-		.detail-in-phone {
-			display: flex;
-			flex-direction: column;
-			justify-content: center;
-			align-items: start;
-			width: 100%;
-		}
-		.detail-in-phone td {
-			display: flex;
-			width: 100%;
-			align-items: center;
-			justify-content: space-between;
-			padding: 1rem;
-			border-bottom: dashed 1px rgba(128, 128, 128, 0.185);
-		}
-		.confirmation-side {
-			width: 100%;
-		}
-		.detail-in-phone input[type='number']::-webkit-inner-spin-button,
-		.detail-in-phone input[type='number']::-webkit-outer-spin-button {
-			-webkit-appearance: none;
-		}
-		.confirmation-side .validation {
-			margin-top: 0rem;
-		}
-		.bottom-Confirmation-side {
-			margin-top: 2rem;
-		}
+	
+
 	}
 </style>
